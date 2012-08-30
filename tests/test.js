33192afb0e12182d2bb32e846542343802da57b1
nathaniel k smith <nathanielksmith@gmail.com>
2012-08-29 23:39:17 -0400

    first pass and stub tests

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+jsdomWrap = require('../lib/nodeunit-jsdom')
+
+exports.testWrapper = {
+    testNoArgs: function(test) {
+        test.done()
+    },
+    testCustomHtml: function(test) {
+        test.done()
+    },
+    testCustomReqs: function(test) {
+        test.done()
+    },
+    testJsdomError: function(test) {
+        test.done()
+    }
+}
